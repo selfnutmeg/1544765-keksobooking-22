ec68f646ad7e209f584f94bc5a8b26d3b7a15bfa
selfnutmeg <step.gor00@icloud.com>
2021-02-12 18:20:24 +0300

    Написал функции получения псевдослучайных чисел

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const getRandomInteger = (min,max) => {
+  if(min>=0 && max>=0 && max>min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return 'Invalid data';
+}
+
+const getRandomFloat = (min,max,decimalDigits=0) => {
+  if(min>=0 && max>=0 && max>min) {
+    return (Math.random() * (max - min) + min).toFixed(decimalDigits);
+  }
+  return 'Invalid data';
+}
